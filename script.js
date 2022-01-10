83ab4bab027eea600ae92a864757c5e68cccb313
Michael Yakovlev <kvadrofilii@gmail.com>
2022-01-11 02:13:44 +0300

    feat: add gett objects & EventListener for buttons

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 'use strict'
 
+// Получаем объекты 
+const slider = document.querySelector('.slider'),
+	sliderWrapper = slider.querySelector('.slider__wrapper'),
+	sliderList = slider.querySelector('.slider__list'),
+	sliderItems = slider.querySelectorAll('.slider__item'),
+	sliderPrev = slider.querySelector('.slider__prev'),
+	sliderNext = slider.querySelector('.slider__next'),
+	// Получаем ширину слайдера
+	slideWidth = sliderItems[0].offsetWidth;
+
+// Номер активного слайда
+let slideIndex = 0;
+
+// Ловим нажатие на кнопку назад
+sliderPrev.addEventListener('click', () => {
+	slideIndex--;
+});
+
+// Ловим нажатие на кнопку вперёд
+sliderNext.addEventListener('click', () => {
+	slideIndex++;
+});
+
+//console.log(slideIndex);
+
